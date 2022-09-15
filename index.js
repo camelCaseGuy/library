10634e0ebc61453d0b6656490be9190331032009
Ryan Hanley <camelCaseGuy@users.noreply.github.com>
2022-09-14 22:15:03 -0500

    Look and feel, dynamically adding books

--- index.js
+++ index.js
@@ -9,13 +9,27 @@ const addBook = (book) => {
 }
 
 function printBooks() {
+  const containerDiv = document.getElementById('container');
+
   library.forEach(book => {
-    console.log(book);
+    const elemDiv = document.createElement('div');
+    elemDiv.innerHTML = book;
+    elemDiv.classList.add('card')
+    containerDiv.appendChild(elemDiv)
   });
 }
 
 addBook('Alice in Wonderland');
 addBook('Treasure Island');
 addBook('Robinson Crusoe');
+addBook('Huckleberry Finn');
+addBook('Little Women');
+addBook('The Invisible Man');
+addBook('20,000 Leagues Under the Sea');
+addBook('Tom Sawyer');
+addBook('The Mysterious Island');
+addBook('Ben Hur');
+addBook('Ivanhoe');
+addBook('The Time Machine');
 
 printBooks();
